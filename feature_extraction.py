27b5b7171ba584fe41b244a6b4374736fdcf5b5e
nnour <nouredine.nour@u-psud.fr>
2018-10-24 17:30:52 +0200

    Before pull

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,4 +1,20 @@
 import numpy as np
-some_text = "We decided to work on the aes project which tackle, the problem of automated essay rating. Such a problem is important to allow student to improve their writting skills"
-tokens = nltk.word_tokenize(some_text)
-print(tokens)
+from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
+import pickle
+
+some_text = "Bonjour, le délai d'acheminement de vos bien est, sur la base des camions! Pourriez vous me contacter, merci."
+X, y = pickle.load(open('data/data_set.pickle', 'rb'))
+# print(len(nltk.word_tokenize(some_text)))
+
+# Ajout de la taille des essais normalisés entre 0 et 1
+X = X[:,None]
+size = []
+for x in X :
+    size.append([len(x[0])])
+size = np.array(size)
+size = size - size.min()
+size = size / size.max()
+X = np.concatenate((X, size), axis=1)
+
+pickle.dump((X,y), open("data/data_featured.pickle", "wb"))
---
+++ test_model.py
@@ -0,0 +1,17 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, accuracy_score
+import numpy as np
+import pickle
+
+X, y = pickle.load(open('data/data_featured.pickle', 'rb'))
+X = X[:,1].reshape(-1, 1)
+y = y.reshape(y.shape[0])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+knn = KNeighborsClassifier(n_neighbors=10)
+knn.fit(X_train, y_train)
+print("Apprentissage terminé")
+pred = knn.predict(X_test)
+print("RMSE : "+str(mean_squared_error(y_test, pred)))
